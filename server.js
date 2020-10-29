4c2c1f88b102759bf16b238cd8ccdda137529e31
Aaron Luing <aaronluing@Aarons-MacBook-Air.local>
2020-10-29 15:48:04 -0500

    set up first controller for the main forum, and added a show route for the forum index.ejs

--- server.js
+++ server.js
@@ -1,42 +1,31 @@
-//___________________
-//Dependencies
-//___________________
+// DEPENDENCIES
 const express = require('express');
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
 const app = express ();
 const db = mongoose.connection;
-//___________________
-//Port
-//___________________
+const forumController = require('./controllers/forum_controller.js');
+app.use(forumController)
+// PORT
 // Allow use of Heroku's port or your own local port, depending on the environment
 const PORT = process.env.PORT || 3000;
 
-//___________________
-//Database
-//___________________
+// DATABASE
 // How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ 'project_2';
-
 // Connect to Mongo
 mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
-
-// Error / success
+// Error / Success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
-
-// open the connection to mongo
+// Open the connection to Mongo
 db.on('open' , ()=>{});
 
-//___________________
-//Middleware
-//___________________
-
-//use public folder for static assets
+// MIDDLEWARE
+// Use public folder for static assets
 app.use(express.static('public'));
-
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+// Populates req.body with parsed info from forms - if no data from forms will return an empty object {}
 app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 
@@ -44,13 +33,12 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
 
+
 //___________________
 // Routes
 //___________________
 //localhost:3000
-app.get('/' , (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', forumController)
 
 //___________________
 //Listener
